f4f35c24283a9a8d493475f08d8fd7f278b17b73
Cmato <martin.cmarko@gmail.com>
2019-08-13 22:32:27 +0200

    Updates

---
+++ precache-manifest.159e9a4aad5a40a1753f9f02dbf1829d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d173b644824db2cfadd17b0a7705130c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "52d02389bdd73ab9fae3",
+    "url": "/monsters-rolodex/static/css/main.24e7af54.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/monsters-rolodex/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "52d02389bdd73ab9fae3",
+    "url": "/monsters-rolodex/static/js/main.66c66216.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
